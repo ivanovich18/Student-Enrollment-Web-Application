19a1d7987a8dc6beb6fc968ac947614784a912a8
ivansuralta <ivangsuralta@gmail.com>
2023-04-03 20:26:52 +0800

    Updated functionalities

--- practice/app.py
+++ practice/app.py
@@ -1,5 +1,7 @@
 from cs50 import SQL
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
 
@@ -27,7 +29,7 @@ def deregister():
     return redirect("/table")
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
 
@@ -42,12 +44,12 @@ def index():
         program = request.form.get("program")
 
         if not id_number or not first_name or not last_name or not birthday or not gender or not student_type or not address or not department or not program:
-            return redirect("/")
+            return redirect("/index")
 
         db.execute("INSERT INTO records (id_number, first_name, last_name, birthday, gender, student_type, address, department, program) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                    id_number, first_name, last_name, birthday, gender, student_type, address, department, program)
 
-        return redirect("/")
+        return redirect("/index")
 
     else:
         return render_template("index.html")
@@ -58,3 +60,39 @@ def table():
     if request.method == "GET":
         records = db.execute("SELECT * FROM records")
         return render_template("table.html", records=records)
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return redirect("/")
+        elif not request.form.get("password"):
+            return redirect("/")
+
+        master_user = db.execute(
+            "SELECT * FROM admin WHERE username = ?", request.form.get("username"))
+        master_pass = db.execute(
+            "SELECT * FROM admin WHERE password = ?", request.form.get("password"))
+
+        if master_user and master_pass:
+            return render_template("index.html")
+
+        else:
+            return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/export", methods=["POST"])
+def export():
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+    sqlquery = "SELECT * FROM records"
+    cursor.execute(sqlquery)
+    result = cursor.fetchall()
+    for row in result:
+        df = pd.read_sql_query(sqlquery, connection)
+        df.to_csv('student_records.csv', index=False)
